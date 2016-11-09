1c0df001891f6a15173d38ad9eccdcd06c0db395
reu_the_joker <reu_the_joker@localhost.localdomain>
2016-11-09 23:55:09 +0200

    name of the database.php in db_object was misspelt

--- db_object.php
+++ db_object.php
@@ -1,5 +1,5 @@
 <?php
-require_once "config/Database.php";
+require_once "config/database.php";
 require_once "class/Database.class.php";
 
 $start = new Database($DB_DSN.$DB, $DB_USER, $DB_PASSWORD);
